0a97cb803ecbf06bd8e9b94fe56bc29573c34d0a
Randomno <emailrandomno@gmail.com>
2023-09-05 09:19:30 +0100

    Create libTASinstall.sh

---
+++ libTASinstall.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+cd ~
+sudo apt update
+sudo apt upgrade
+curl -s https://api.github.com/repos/clementgallet/libtas/releases/latest | grep -Po "(?<=\"browser_download_url\": \").*_amd64.*(?=\")" | xargs wget
+sudo apt install -y ./libtas*.deb
+export WAYLAND_DISPLAY=""
+echo 'export WAYLAND_DISPLAY=""' >> .bashrc
