817fc49d143173014dee402c3744218cd2dc4bec
Randomno <emailrandomno@gmail.com>
2023-09-05 23:53:16 +0100

    set variable in bash_profile instead of bashrc

--- libTASinstall.sh
+++ libTASinstall.sh
@@ -6,4 +6,4 @@ sudo apt upgrade
 curl -s https://api.github.com/repos/clementgallet/libtas/releases/latest | grep -Po "(?<=\"browser_download_url\": \").*_amd64.*(?=\")" | xargs wget
 sudo apt install -y ./libtas*.deb
 export WAYLAND_DISPLAY=""
-echo 'export WAYLAND_DISPLAY=""' >> .bashrc
+echo 'export WAYLAND_DISPLAY=""' >> .bash_profile
